1731dbd06a8036a65f120ffc6f2ff3eba75aebac
asharifo <asharifo@student.ubc.ca>
2025-07-04 14:41:34 +0700

    Hero page edits

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 window.addEventListener('load', () => {
     const navbar = document.querySelector('.navbar');
     const hero = document.querySelector('.hero');
-
-    // Expand navbar
     navbar.classList.add('open');
-
-    // Reveal hero content after navbar expands
-    setTimeout(() => {
-        hero.classList.add('show');
-    }, 1200);
+    hero.classList.add('show');
 });
